dbfd34f57fb687ffbec27a62fe08669502c6e570
Quang Minh <139391994+MinhQuang02@users.noreply.github.com>
2025-12-26 19:53:24 +0700

    Intial Commit

--- src/application/server/index.js
+++ src/application/server/index.js
@@ -24,8 +24,10 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
 export default app; // For Vercel
